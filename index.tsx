1c4a2c623fe620bc15b4f9dad4cfa4fc3bfb03b3
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-09-13 16:21:42 -0400

    Fix everything

--- index.tsx
+++ index.tsx
@@ -1,29 +1,57 @@
 import { Hono } from "hono";
-import type { FC } from "hono/jsx";
 
 const app = new Hono();
 
-const Layout: FC = (props) => {
+function Script({ type, children }: { type: string; children: string }) {
+  return (
+    <script
+      type={type}
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: Intentional!
+      dangerouslySetInnerHTML={{ __html: children }}
+    />
+  );
+}
+
+function _Layout(props: { children: any }) {
   return (
     <html lang="en">
-      <body>{props.children}</body>
+      <head>
+        <Script type="importmap">
+          {JSON.stringify(
+            {
+              imports: {
+                "hono/jsx": "https://esm.sh/hono/jsx",
+                "hono/jsx/dom": "https://esm.sh/hono/jsx/dom",
+                "hono/css": "https://esm.sh/hono/css",
+              },
+            },
+            null,
+            2,
+          )}
+        </Script>
+      </head>
+      <body>
+        {props.children}
+        <script type="module" src="/main.mjs"></script>
+      </body>
     </html>
   );
-};
+}
 
-const Top: FC<{ messages: string[] }> = (props: { messages: string[] }) => {
+function Top(props: { messages: string[] }) {
   return (
-    <Layout>
-      <h1>Hello Hono!</h1>
-      <script type="module" src="/main.mjs"></script>
-      <ul>
-        {props.messages.map((message) => {
-          return <li>{message}!!</li>;
-        })}
-      </ul>
-    </Layout>
+    <_Layout>
+      <Script type="module">{`window.__initialProps = ${JSON.stringify(props)}`}</Script>
+      <main id="root"></main>
+      {/* <h1>Hello Hono!</h1>
+        <ul>
+          {props.messages.map((message) => {
+            return <li>{message}!!</li>;
+          })}
+        </ul> */}
+    </_Layout>
   );
-};
+}
 
 app.get("/", (c) => {
   const messages = ["Good Morning", "Good Evening", "Good Night"];
--- public/main.mjs
+++ public/main.mjs
@@ -1,1 +1,30 @@
-console.log("Hello, world!");
+import { createElement, Fragment, render } from "hono/jsx/dom";
+
+function h(...args) {
+  let [type] = args;
+  if (args.length === 1) {
+    return createElement(type, {}, null);
+  } else if (args.length === 2) {
+    let [type, propsOrChildren] = args;
+    return createElement(type, {}, ...propsOrChildren);
+  } else {
+    let [, props, ...children] = args;
+    return createElement(type, props, ...children);
+  }
+}
+
+let root = document.querySelector("#root");
+
+function App(props = window.__initialProps) {
+  return h(Fragment, [
+    h("h1", "Hello Hono!"),
+    h(
+      "ul",
+      props.messages.map((message) => {
+        return h("li", message);
+      }),
+    ),
+  ]);
+}
+
+render(h(App, window.__initialProps, null), root);
